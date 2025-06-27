6afe29965c2ba81217faa6489b4e830cbf09d452
zaira78 <innocentadnan256@gmail.com>
2025-06-27 19:48:11 +0500

    Update 0aaryan2.js

--- Priyansh/commands/0aaryan2.js
+++ Priyansh/commands/0aaryan2.js
@@ -12,12 +12,12 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("💋")==0 || event.body.indexOf("🥵")==0 || event.body.indexOf("💦")==0 || event.body.indexOf("🤤")==0) {
+  if (event.body.indexOf("👄")==0 || event.body.indexOf("🥵")==0 || event.body.indexOf("💦")==0 || event.body.indexOf("🤤")==0) {
     var msg = {
-        body: "💐𝐁𝐡𝐚𝐠 𝐘𝐚𝐡𝐚 𝐬𝐞 𝐓𝐡𝐚𝐫𝐤𝐢 𝐈𝐧𝐬𝐚𝐚𝐧😠💐",
+        body: "💐𝐁𝐡𝐚𝐠 𝐘𝐚𝐡𝐚 𝐬𝐞 𝐓𝐡𝐚𝐫𝐤𝐢 𝐈𝐧𝐬𝐚𝐚𝐧😾💐",
       }
       api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🤬", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😾", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
