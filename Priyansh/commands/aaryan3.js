2e66f817cca8d64398fbbfac42f7236601b0f9b0
zaira78 <innocentadnan256@gmail.com>
2025-06-28 19:50:22 +0500

    Update aaryan3.js

--- Priyansh/commands/aaryan3.js
+++ Priyansh/commands/aaryan3.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("😅") ||
-     react.includes("🤣") || react.includes("😀") || react.includes("😃") ||
+     react.includes("🤣") || react.includes("😂") || react.includes("😃") ||
 react.includes("😄") ||
 react.includes("😹")) {
 		var msg = {
