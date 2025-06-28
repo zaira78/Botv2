22890faac956adf7eec8bb17da019ef9152b607e
zaira78 <innocentadnan256@gmail.com>
2025-06-28 20:31:03 +0500

    Update gali_reply.js

--- Priyansh/commands/gali_reply.js
+++ Priyansh/commands/gali_reply.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bsdk")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("maa ki chut")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("behen chod")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("Bsdke")==0 || event.body.indexOf("gandu")==0) {
+	if (event.body.indexOf(".fuck")==0 || event.body.indexOf("Mc")==0 || event.body.indexOf("chodu")==0 || event.body.indexOf("Bsdk")==0 || event.body.indexOf("Bc")==0 || event.body.indexOf("Maa ki chut")==0 || event.body.indexOf("Tmkc")==0 || event.body.indexOf("bhenchod")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("Madarchod")==0 || event.body.indexOf("Bsdk")==0 || event.body.indexOf("Gandu")==0) {
 		var msg = {
-				body: "(Boss Dk , Gali q Dete Ho. Lund Katke Hath M rakh Dungi)",
+				body: "(Gali q Dete Ho. Lund Katke Hath M rakh Dungi)",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
