bc752d21bdf1432e2224faf6c24f0d556c0dc07c
zaira78 <innocentadnan256@gmail.com>
2025-06-30 12:34:07 +0500

    Update 0aaryan.js

--- Priyansh/commands/0aaryan.js
+++ Priyansh/commands/0aaryan.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("😢")==0 || event.body.indexOf("😭")==0 || event.body.indexOf("😔")==0 || event.body.indexOf("🥺")==0) {
+  if (event.body.indexOf("😞")==0 || event.body.indexOf("😭")==0 || event.body.indexOf("🙁")==0 || event.body.indexOf("🥺")==0) {
     var msg = {
         body: "💐𝐊𝐲𝐚 𝐇𝐮𝐚 𝐁𝐚𝐛𝐲 𝐬𝐚𝐝 𝐌𝐚𝐭 𝐡𝐨 𝐌𝐚𝐢 𝐇𝐮 𝐧𝐚 💐",
       }
