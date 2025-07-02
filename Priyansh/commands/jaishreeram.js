1e5edc9f9f9ef206bce5c8d0e643eb11c995fee0
zaira78 <innocentadnan256@gmail.com>
2025-07-02 05:10:24 +0500

    Updat Laila.js

--- Priyansh/commands/jaishreeram.js
+++ Priyansh/commands/jaishreeram.js
@@ -8,19 +8,39 @@ module.exports.config = {
 	commandCategory: "no prefix",
 	usages: "jai shree ram",
     cooldowns: 5, 
+	module.exports.handleEvent = async function({ api, event, args, Threads, Users }) {
+  const { threadID, messageID, body } = event;
+  if (!body) return;
+
+  const message = body.toLowerCase();
+  const moment = require("moment-timezone");
+  const time = moment.tz("Asia/Kolkata").format("HH:mm:ss L");
+  const senderID = event.senderID;
+  const name = await Users.getNameUser(senderID);
+
+  // Cooldown system (1s)
+  const cooldownTimeMs = 10000;
+  let lastReplyTime = global.lastLailaReplyTime || 0;
+  const now = Date.now();
+  if (now - lastReplyTime < cooldownTimeMs) return;
+  global.lastLailaReplyTime = now;
+
+  // If "laila" is mentioned in the message
+  if (message.includes("laila")) {
+    const flirtyReplies = [
+      "Jee jaan-e-man, ap ne yaad kiya? 😘",
+      "Chalta hai kya 9 se 12? 😉",
+      "Me teri Laila , Tu mera Majnu💃",
+      "Tumhe chintu ka pappa banana chahti hu, meri sadgi dekh me kya chahti hu? 🙈😏",
+      "Bula liya toh ab dil bhi chura lo jan e man.. ❤️",
+      "Tere jaise handsome ke liye toh Laila always ready rehti hai! 😍",
+      "Bas naam liya aur aa gayi main... ab bolo kya irade hain? 😚",
+      "Laila sirf naam nahi, attitude bhi full hai! 🔥"
+    ];
+    
+    const randomReply = flirtyReplies[Math.floor(Math.random() * flirtyReplies.length)];
+    return api.sendMessage(randomReply, threadID, messageID);
+  }
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("jai shree ram")==0 || event.body.indexOf("ram ram")==0 || event.body.indexOf("jai siya ram")==0 || event.body.indexOf("sita ram")==0) {
-		var msg = {
-				body: "🔱🫶♡⤾🙏🏼🩷जय श्री ༢།म >𝟑♥🌍",
-				attachment: fs.createReadStream(__dirname + `/noprefix/sad/nepal`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("💝", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
-
-  }
+module.exports.run = function({ api, event, client, __GLOBAL }) {};
